94b96d375a7a949bb7caeb64d55a524cb4e0cfca
Harsh Icecreamwala <icecreamwalaharsh@gmail.com>
2026-06-09 19:42:42 +0530

    Add files via upload

--- fetch_rss.py
+++ fetch_rss.py
@@ -30,10 +30,9 @@ RSS_FEEDS = [
     }
 ]
 
-
 def save_article(article):
     try:
-        result = supabase.table("articles").insert(article).execute()
+        result = supabase.table("articles").upsert(article,on_conflict="link").execute()
         print(f"Saved: {article['title']}")
         print(result)
 
@@ -41,10 +40,68 @@ def save_article(article):
         print(f"ERROR inserting article: {article['title']}")
         print(str(e))
 
+def extract_image(entry):
+    # media_thumbnail
+    if "media_thumbnail" in entry:
+        return entry.media_thumbnail[0]["url"]
+
+    # media_content
+    if "media_content" in entry:
+        return entry.media_content[0]["url"]
+
+    # enclosure
+    if "links" in entry:
+        for link in entry.links:
+            if link.get("type", "").startswith("image"):
+                return link.get("href")
+
+    return None
+
+def detect_category(title):
+    title = title.lower()
+
+    sports = [
+        "cricket", "football", "ipl",
+        "fifa", "tennis", "nba",
+        "match", "sports"
+    ]
+
+    entertainment = [
+        "movie", "film", "actor",
+        "actress", "bollywood",
+        "hollywood", "music"
+    ]
+
+    technology = [
+        "ai", "artificial intelligence",
+        "tech", "software",
+        "google", "microsoft",
+        "apple"
+    ]
+
+    business = [
+        "stock", "market",
+        "economy", "business",
+        "startup", "finance"
+    ]
+
+    if any(word in title for word in sports):
+        return "Sports"
+
+    if any(word in title for word in entertainment):
+        return "Entertainment"
+
+    if any(word in title for word in technology):
+        return "Technology"
+
+    if any(word in title for word in business):
+        return "Business"
+
+    return "General"
 
 def process_feed(feed_name, feed_url):
     feed = feedparser.parse(feed_url)
-
+    
     print(f"Feed: {feed_name}")
     print(f"Entries found: {len(feed.entries)}")
 
@@ -55,12 +112,17 @@ def process_feed(feed_name, feed_url):
     for entry in feed.entries:
         print(f"Processing: {entry.get('title', 'NO TITLE')}")
 
+        category = detect_category(entry.get("title", ""))
+        image_url = extract_image(entry)
+        
         article = {
             "title": entry.get("title", ""),
             "link": entry.get("link", ""),
             "summary": entry.get("summary", ""),
             "source": feed_name,
-            "published_at": datetime.utcnow().isoformat()
+            "category": category,
+            "image_url": image_url,
+            "published_at": entry.get("published", None)
         }
 
         save_article(article)
